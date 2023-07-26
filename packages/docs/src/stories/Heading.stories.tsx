f9ea768d791fc387f5daabec7b047e301714d3be
Guilherme Ciarlini <guilhermeciarlini@gmail.com>
2023-07-26 16:55:37 -0300

    change ignite-ui to ciarlini-ignite-ui

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -1,5 +1,5 @@
 import { StoryObj, Meta } from '@storybook/react'
-import { Heading, HeadingProps } from '@ignite-ui/react'
+import { Heading, HeadingProps } from '@ciarlini-ignite-ui/react'
 
 export default {
   title: 'Typography/Heading',
