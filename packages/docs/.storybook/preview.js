30b57af11640710fa864b654bb4faf022afc21ad
Guilherme Ciarlini <guilhermeciarlini@gmail.com>
2023-07-26 16:13:15 -0300

    downgrade storybook version

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,30 +1,14 @@
 import { themes } from '@storybook/theming'
 
-/** @type { import('@storybook/react').Preview } */
-const preview = {
-  parameters: {
-    parameters: {
-      backgrounds: {
-        default: 'dark',
-        values: [
-          {
-            name: 'dark',
-            value: '#1B1C1D'
-          }
-        ]
-      }
-    },
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/
-      }
-    },
-    docs: {
-      theme: themes.dark
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
     }
+  },
+  docs: {
+    theme: themes.dark
   }
 }
-
-export default preview
